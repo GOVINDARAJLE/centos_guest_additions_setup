1f08168635d173093b5835d0ee30b4cc0b05a81e
GOVINDA RAJLE <48848473+GOVINDARAJLE@users.noreply.github.com>
2021-12-02 08:13:58 +0530

    Update

---
+++ guest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+yum -y install epel-release
+
+
+yum -y update
+yum -y upgrade
+
+yum install make gcc kernel-headers kernel-devel perl dkms bzip2
+
+export KERN_DIR=/usr/src/kernels/$(uname -r)
+
+mount -r /dev/cdrom /media
+
+cd /media/
+
+./VBoxLinuxAdditions.run
+
+
+
